eb2a7fff8f727c96a9eb6a214c95df87513f4c00
Askforthemoon <classwizard@163.com>
2022-05-12 15:50:46 +0800

    First Commit

--- src/main/java/com/kembl/Controller/StudentController2.java
+++ src/main/java/com/kembl/Controller/StudentController2.java
@@ -9,37 +9,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 //@RestController
-@RequestMapping("/students")
-public class StudentController2 {
-    @Autowired
-    private IStudentService studentService;
-
-    @GetMapping
-    public List<Student> getAll(){
-        return studentService.list();
-    }
-
-    @PostMapping
-    public Boolean save(@RequestBody Student student){
-        return studentService.save(student);
-    }
-
-    @PutMapping
-    public Boolean update(@RequestBody Student student){
-        return studentService.updateById(student);
-    }
-
-    @DeleteMapping("{id}")
-    public Boolean delete(@PathVariable Integer id){
-        return studentService.removeById(id);
-    }
-
-    @GetMapping("{id}")
-    public Student getById(@PathVariable Integer id){
-        return studentService.getById(id);
-    }
-    @GetMapping("{currentPage}/{pageSize}")
-    public IPage<Student> getPage(@PathVariable int currentPage, @PathVariable int pageSize){
-        return studentService.getPage(currentPage, pageSize);
-    }
-}
+//@RequestMapping("/students")
+//public class StudentController2 {
+//    @Autowired
+//    private IStudentService studentService;
+//
+//    @GetMapping
+//    public List<Student> getAll(){
+//        return studentService.list();
+//    }
+//
+//    @PostMapping
+//    public Boolean save(@RequestBody Student student){
+//        return studentService.save(student);
+//    }
+//
+//    @PutMapping
+//    public Boolean update(@RequestBody Student student){
+//        return studentService.updateById(student);
+//    }
+//
+//    @DeleteMapping("{id}")
+//    public Boolean delete(@PathVariable Integer id){
+//        return studentService.removeById(id);
+//    }
+//
+//    @GetMapping("{id}")
+//    public Student getById(@PathVariable Integer id){
+//        return studentService.getById(id);
+//    }
+//    @GetMapping("{currentPage}/{pageSize}")
+//    public IPage<Student> getPage(@PathVariable int currentPage, @PathVariable int pageSize){
+//        return studentService.getPage(currentPage, pageSize);
+//    }
+//}
